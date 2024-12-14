e3de3146a6fdaeb12f8cb1086b00ec11af162113
Vobh <vobh_malino@hotmail.com>
2024-12-14 15:15:05 -0300

    Continue 02

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -205,6 +205,35 @@ const Page = () => {
             </div>
 
             {/* fourth bento grid element */}
+            <div className="relative lg:row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] 
+              lg:rounded-r-[2rem]" />
+
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)]
+              max-lg:rounded-b-[calc(2rem+1px)] lg-rounded-r-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Easy Integration
+                  </p>
+                  <p className="mt-2 max-w-lg text-sm/6 text-gray-600 max-lg:text-center">
+                    Connect PingPanda with your existing workflow in munutes and 
+                    call our intuitive logging API from any language.
+                  </p>
+                </div>
+
+                <div className="relative min-h-[30rem] w-full grow">
+                    <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden
+                    rounded-tl-xl bg-gray-900 shadow-2xl">
+                      <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                        <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                          <div className="border-b border-r border-b-white/20 border-r-white/10
+                          bg-white/5 px-4 py-2 text-white">pingpanda.js</div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+              </div>
+            </div>
           </div>
         </MaxWidthWrapper>
       </section>
