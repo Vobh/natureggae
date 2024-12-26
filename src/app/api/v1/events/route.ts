9354dfa71f6dbda895ba879321668de5c85d91c5
Vobh <vobh_malino@hotmail.com>
2024-12-26 11:36:05 -0300

    Bug in progress

--- src/app/api/v1/events/route.ts
+++ src/app/api/v1/events/route.ts
@@ -2,7 +2,10 @@ import { FREE_QUOTA, PRO_QUOTA } from "@/config";
 import { db } from "@/db";
 import { DiscordClient } from "@/lib/discord-client";
 import { CATEGORY_NAME_VALIDATOR } from "@/lib/validators/category-validator";
+import { Description } from "@radix-ui/react-dialog";
+import { timeStamp } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import { describe } from "node:test";
 import { z } from "zod";
 
 const REQUEST_VALIDATOR = z
@@ -75,4 +78,100 @@ export const Post = async (req: NextRequest) => {
     }
 
     const discord = new DiscordClient(process.env.DISCORD_BOT_TOKEN)
+
+    const dmChannel = await discord.createDM(user.discordId)
+
+    let requestData: unknown
+
+    try {
+        requestData = await req.json()
+    } catch (err) {
+        return NextResponse.json(
+            {
+                message: "Invalid JSON request body",
+            },
+            { status: 400 }
+        )
+    }
+
+    const validationResult = REQUEST_VALIDATOR.parse(requestData)
+
+    const category = user.EventCategories.find((cat) => cat.name === validationResult.category
+    )
+
+    if(!category) {
+        return NextResponse.json(
+            {
+                message: `You dont have a category named "${validationResult.category}"`,
+            },
+            { status: 404 }
+        )
+    }
+
+    const eventData = {
+        title: `${category.emoji || "👥"} ${
+            category.name.charAt(0).toUpperCase() + category.name.slice(1)
+
+        }`,
+        Description: validationResult.description || `A new ${category.name} event has occurred!`,
+        color: category.color, 
+        timeStamp: new Date().toISOString(),
+        fields: Object.entries(validationResult.fields || {}).map(([key, value]) => {
+            return {
+                name: key,
+                value: String(value),
+                inline: true,
+            }
+        }),
+    }
+
+    const event = await db.event.create({
+        data: {
+            name: category.name,
+            formattedMessage: `${eventData.title}/n/n${eventData.Description}`,
+            userId: user.id,
+            fields: validationResult.fields || {},
+            eventCategoryId: category.id,
+        },
+    })
+
+    try {
+        await discord.sendEmbed(dmChannel.id, eventData)
+
+        await db.event.update({
+            where: { id: event.id },
+            data: { deliveryStatus: "DELIVERED" },
+        })
+
+        await db.quota.upsert({
+            where: { userId: user.id, month: currentMonth, year: currentYear },
+            update: { count: { increment: 1 } },
+            create: {
+                userId: user.id,
+                month: currentMonth,
+                year: currentYear,
+                count: 1,
+            }
+        })
+    } catch (err) {
+        await db.event.update({
+            where: { id: event.id },
+            data: { deliveryStatus: "FAILED" },
+        })
+
+        console.log(err)
+
+        return NextResponse.json(
+            {
+                message: "Error processing event",
+                eventId: event.id,
+            }, 
+            { status: 500 }
+        )
+    }
+
+    return NextResponse.json({
+        message: "Event processed successfully",
+        eventId: event.id,
+    })
 } 
--- src/lib/discord-client.ts
+++ src/lib/discord-client.ts
@@ -1,5 +1,10 @@
 import { REST } from "@discordjs/rest"
-
+import {
+    RESTPostAPIChannelMessageResult,
+    RESTPostAPICurrentUserCreateDMChannelResult,
+    Routes,
+    APIEmbed
+} from "discord-api-types/v10"
 export class DiscordClient {
     private rest: REST
 
@@ -7,5 +12,18 @@ export class DiscordClient {
         this.rest = new REST({version: "10"}).setToken(token ?? "")
     }
 
-    
+    async createDM(
+        userId: string
+    ): Promise<RESTPostAPICurrentUserCreateDMChannelResult> {
+        return this.rest.post(Routes.userChannels(), {
+                body: { recipient_id: userId },
+            }) as Promise<RESTPostAPICurrentUserCreateDMChannelResult>        
+    }
+
+    async sendEmbed(channelId: string, embed: APIEmbed): 
+    Promise<RESTPostAPICurrentUserCreateDMChannelResult> {
+        return this.rest.post(Routes.channelMessages(channelId), {
+            body: { embeds: [embed] },
+        }) as Promise<RESTPostAPICurrentUserCreateDMChannelResult>
+    }
 }
