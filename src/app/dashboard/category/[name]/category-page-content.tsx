558f9947a12bd4805670b4da6dd5ab5bf1fa1c4b
Vobh <vobh_malino@hotmail.com>
2025-01-04 14:54:16 -0300

    Implements Checkout

--- src/app/dashboard/category/[name]/category-page-content.tsx
+++ src/app/dashboard/category/[name]/category-page-content.tsx
@@ -14,7 +14,7 @@ import { ColumnDef, ColumnFiltersState, flexRender, getCoreRowModel, getFiltered
 import { Button } from "@/components/ui/button";
 import { cn } from "@/utils";
 import { Heading } from "@/components/heading";
-import { Table, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 interface CategoryPageContentProps {
     hasEvents: boolean
@@ -288,8 +288,55 @@ const NumericFieldSumCards = () => {
                                 </TableRow>
                             ))}
                         </TableHeader>
+
+                        <TableBody>
+                            {isFetching ? (
+                                [...Array(5)].map((_, rowIndex) => (
+                                <TableRow key={rowIndex}>
+                                    {columns.map((_, cellIndex) => (
+                                        <TableCell key={cellIndex}>
+                                            <div className="h-4 w-full bg-gray-200 animate-pulse rounded"/>
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                                ))
+                            ) : table.getRowModel().rows.length ? (
+                                table.getRowModel().rows.map((row) => (
+                                    <TableRow key={row.id}>
+                                        {row.getVisibleCells().map((cell) => (
+                                            <TableCell key={cell.id}>
+                                                {flexRender(cell.column.columnDef.cell, cell.getContext()
+                                            )}
+                                            </TableCell>
+                                        ))}
+                                    </TableRow>
+                                ))
+                            ) : ( 
+                            <TableRow>
+                                <TableCell colSpan={columns.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                            )}
+                        </TableBody>
                     </Table>
                 </Card>
+            </div>
+
+            <div className="flex items-center justify-end space-x-2 py-4">
+                <Button variant="outline" size="sm" onClick={() => table.previousPage()}
+                    disabled={!table.getCanPreviousPage() || isFetching}
+                >
+                    Previous
+                </Button>
+                <Button 
+                    variant="outline" 
+                    size="sm" 
+                    onClick={() => table.nextPage()}
+                    disabled={!table.getCanNextPage() || isFetching}
+                >
+                    Next
+                </Button>
             </div>
         </div>
     )
